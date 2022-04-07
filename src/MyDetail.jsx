e0052991ebb82ddde4a376558dd382697d7271f7
Abhishek Arya <abhishek13arya@gmail.com>
2022-04-07 22:31:42 +0530

    icon set kiye

--- src/MyDetail.jsx
+++ src/MyDetail.jsx
@@ -11,9 +11,9 @@ function MyDetails (){
     <H1>Projects</H1>
     </div>
     <Box>
-      <DetailBox icon="https://www.nicepng.com/png/full/177-1779357_got-a-question-call-1-877-858-white.png">Contact Phone <span className="flex">Number</span></DetailBox>
-      <DetailBox icon="https://www.pngkit.com/png/full/222-2226274_white-email-icon-png-download-mail-symbol-png.png">Our Email <span className="flex">Address</span></DetailBox>
-      <DetailBox icon="https://openday.runshaw.ac.uk/wp-content/uploads/2020/10/Destination-Pin.png">Our Location <span className="flex">Address</span></DetailBox>
+      <DetailBox icon="https://www.nicepng.com/png/full/177-1779357_got-a-question-call-1-877-858-white.png">Phone <span className="flex">Number</span></DetailBox>
+      <DetailBox icon="https://www.pngkit.com/png/full/222-2226274_white-email-icon-png-download-mail-symbol-png.png">mail <span className="flex">Address</span></DetailBox>
+      <DetailBox icon="https://openday.runshaw.ac.uk/wp-content/uploads/2020/10/Destination-Pin.png">Location <span className="flex">Address</span></DetailBox>
     </Box>
     </div>
   );
