10c2d10bb7dfdf3789c7d8d37fdd7e51563580fe
Abhishek Arya <abhishek13arya@gmail.com>
2022-06-08 12:42:44 +0530

    update resume

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
                     <H1>WELCOME TO MY WORLD</H1>
                     <span className="text-6xl md:text-4xl lg:text-6xl sm:text-6xl font-bold bg-clip-text bg-gradient-to-b from-indigo-500 to-pink-500 text-transparent p-2">Hi, I’m Abhishek Arya</span>
                     <span className="text-5xl sm:text-5xl font-bold bg-clip-text bg-gradient-to-b from-indigo-500 to-pink-500 text-transparent p-2">Web Designer & Developer.</span>
-                    <button className="bg-yellow-400 border-2 border-yellow-700 text-white font-bold p-2 rounded-md w-24 hover:bg-yellow-500"><a target="_blank" href="https://drive.google.com/file/d/1znqDuUMgWJAslDwivfJWznXcAny37Feu/view">Resume</a></button>
+                    <button className="bg-yellow-400 border-2 border-yellow-700 text-white font-bold p-2 rounded-md w-24 hover:bg-yellow-500"><a target="_blank" href="https://drive.google.com/file/d/1PUVRb1Sd4XEd2eAp3enL9PqRCuZOl3iC/view">Resume</a></button>
 
                 </div>
             </div>
