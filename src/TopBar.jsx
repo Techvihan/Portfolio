25e84077b9f37b8b9b694aa332fe43b7820c7938
Abhishek Arya <abhishek13arya@gmail.com>
2022-06-24 11:35:46 +0530

    top bar navigation

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -7,10 +7,10 @@ function TopBar() {
       <div className="flex w-screen py-2 z-50 justify-between px:6 sm:px-10 bg-gray-700">
         <H2>TechVihan</H2>
         <div className="flex space-x-4 text-white ml-8">
-          <a href="home">Home</a>
-          <a href="skill">Skills</a>
-          <a href="project">Projects</a>
-          <a href="detail">Details</a>
+          <a href="#home">Home</a>
+          <a href="#skill">Skills</a>
+          <a href="#project">Projects</a>
+          <a href="#detail">Details</a>
         </div>
       </div>
     </div>
