620926c3cce75faef88052ade7ace144e003b15c
Abhishek Arya <abhishek13arya@gmail.com>
2022-06-24 11:48:49 +0530

    top bar width

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="bg-hero-pattern">
       <Container>
-
       </Container>
     </div>
   );
--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -4,9 +4,9 @@ import H2 from './H2';
 function TopBar() {
   return (
     <div>
-      <div className="flex w-screen py-2 z-50 justify-between px:6 sm:px-10 bg-gray-700">
+      <div className="flex justify-between px-2 py-2 sm:px-10 bg-gray-700">
         <H2>TechVihan</H2>
-        <div className="flex space-x-4 text-white ml-8">
+        <div className="flex space-x-4 text-white ">
           <a href="#home">Home</a>
           <a href="#skill">Skills</a>
           <a href="#project">Projects</a>
